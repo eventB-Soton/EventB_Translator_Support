ef4a62d2586b46b6fd738ea9f01ac9d527e34ffe
cfsnook <cfs@ecs.soton.ac.uk>
2018-01-09 15:23:52 +0000

    perform save in a RodinCore runnable

--- ac.soton.emf.translator.eventb/src/ac/soton/emf/translator/eventb/handler/EventBTranslateHandler.java
+++ ac.soton.emf.translator.eventb/src/ac/soton/emf/translator/eventb/handler/EventBTranslateHandler.java
@@ -3,19 +3,24 @@
  */
 package ac.soton.emf.translator.eventb.handler;
 
-import java.io.IOException;
-import java.util.Collections;
-
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eventb.emf.persistence.EMFRodinDB;
-import org.eventb.emf.persistence.PersistencePlugin;
 import org.eventb.emf.persistence.SaveResourcesCommand;
 import org.rodinp.core.IInternalElement;
+import org.rodinp.core.RodinCore;
 
 import ac.soton.emf.translator.handler.TranslateHandler;
 
@@ -32,6 +37,9 @@ import ac.soton.emf.translator.handler.TranslateHandler;
  */
 public class EventBTranslateHandler extends TranslateHandler {	
 	
+	private final String pluginID = "ac.soton.emf.translator.eventb";
+	private final String errorMessage = "failed saving resources after translation";
+	
 	@Override
 	protected EObject getEObject (Object obj){
 		if (obj instanceof IInternalElement){
@@ -42,34 +50,53 @@ public class EventBTranslateHandler extends TranslateHandler {
 	}
 	
 	/**
-	 * Use the SaveResourcesCommand to persist into Rodin
+	 * Use the SaveResourcesCommand to persist emf resources
+	 * This must be done in a RodinCore runnable
 	 * 
 	 * @param editingDomain
 	 * @param monitor
 	 * @throws ExecutionException 
 	 */
 	@Override
-	protected void doSave(final TransactionalEditingDomain editingDomain, IProgressMonitor monitor) throws Exception {
-		// delete empty modified resources 	
-		for (Resource resource : editingDomain.getResourceSet().getResources()) {
-			try {
-				if (resource.getContents().isEmpty() && resource.isModified()){
-					resource.eSetDeliver(false); 
-					resource.delete(Collections.emptyMap());
-				}
-				monitor.worked(2);
-			} catch (IOException e) {
-				e.printStackTrace();
-				Status status = new Status(Status.ERROR, PersistencePlugin.PLUGIN_ID, "IO Exception while saving resource : " + resource.getURI() + " :- \n" + e.getMessage(), e);
-				PersistencePlugin.getDefault().getLog().log(status);
-			}
-		}
-		// save all resources that have been modified					
-		SaveResourcesCommand saveCommand = new SaveResourcesCommand(editingDomain);
+	protected void save(final TransactionalEditingDomain editingDomain, IProgressMonitor monitor) throws Exception {
+		// save all resources that have been modified	
+		final SaveResourcesCommand saveCommand = new SaveResourcesCommand(editingDomain);
 		if (saveCommand.canExecute()){
-				saveCommand.execute(monitor, null);
+			RodinCore.run(new IWorkspaceRunnable() {
+				public void run(final IProgressMonitor monitor) throws CoreException {
+
+					try {
+						saveCommand.execute(monitor, null);
+					} catch (ExecutionException e) {
+						IStatus status = new Status(IStatus.ERROR, pluginID , errorMessage , e);
+						throw new CoreException(status);
+					}
+
+				}
+			}, getSchedulingRule(editingDomain.getResourceSet().getResources().toArray(new Resource[0])), monitor);
 		}
 		monitor.done();
 	}
 	
+	private ISchedulingRule getSchedulingRule(Resource[] resources) {
+		if (resources.length==0){	
+			return null;
+		}else if (resources.length==1){
+			return WorkspaceSynchronizer.getFile(resources[0]);
+		}else {
+			IProject project = getProject(resources[0]);
+			for (Resource resource : resources) {
+				if (project != getProject(resource)){
+					return  ResourcesPlugin.getWorkspace().getRoot();
+				}
+			}
+			return project;
+		}
+	}
+	
+	private IProject getProject(Resource resource) {
+		IFile file = WorkspaceSynchronizer.getFile(resource);
+		return file==null? null : file.getProject();
+	}
+
 }
